c1fd441a8c9e1c483f29f963a9aa6fc30c352d7c
adriamarticomas <adriamarticomas@192.168.1.137>
2022-07-05 16:37:02 +0200

    ex3

---
+++ S01T04N01MartiComasAdria/src/exercici3/OutOfBounds.java
@@ -0,0 +1,14 @@
+package exercici3;
+
+import java.util.ArrayList;
+
+public class OutOfBounds {
+	ArrayList<String> list = new ArrayList<String>();
+	
+	String x = list.get(1);
+
+}
+
+/*
+ * 
+ */
